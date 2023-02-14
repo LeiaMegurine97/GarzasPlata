1e7d406e3cb1ed84f744b67446a25794b7eaf51e
Jessica Lopez <lo399651@uaeh.edu.mx>
2023-02-14 13:06:47 -0600

    Interfaz Login

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset ="utf-8">
+        <title> Login </title>
+        <link rel="stylesheet" href="CSS/Estilos.css">
+
+    </head>
+    <body>
+        <div class="loginBox">
+            <img src="Imagenes/Garza.png" alt="Logo Garza">
+            <h1> Login Here</h1>
+            <form>
+                <!--NO CUENTA -->
+                <label for="NoCuenta">No. Cuenta</label>
+                <input type="text" placeholder="Ingresa tu No. Empleado">
+                
+                <!-- NIP -->
+                <label for="Nip">NIP</label>
+                <input type="password" placeholder="Ingresa tu NIP">
+
+                <input type="sumit" value="Log In">
+
+                <a href="#">Olvidaste tu NIP</a>
+                <a href="#">No tienes una cuenta</a>
+
+            </form>
+        </div>
+    </body>
+</html>
+
+<?php
+
+
+
+
+
+
+
+?>
